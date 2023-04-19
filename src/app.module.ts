156a521339577bd7eca36bee5d3e7132e064935d
Trung Vu <trungdvu.it@gmail.com>
2023-04-19 10:48:39 +0700

    feat: env vars validation (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from '../db/data-source';
+import { configModuleOptions } from './configs/config-module-options.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
+    ConfigModule.forRoot(configModuleOptions),
     TypeOrmModule.forRoot(dataSourceOptions),
     UserModule,
     AuthModule,
