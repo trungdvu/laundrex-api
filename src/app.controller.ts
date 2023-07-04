b479703fbbd6267f8e526b73ba841d176afb4656
Trung Vu <trungdvu.it@gmail.com>
2023-07-04 21:30:57 +0700

    disable https

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,8 @@
-import { Controller, Get, Res } from '@nestjs/common';
-import { Response } from 'express';
-import { resolve } from 'path';
+import { Controller, Get } from '@nestjs/common';
 import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AppController {
-  @Public()
-  @Get('.well-known/pki-validation/6DC64225FDA8C517652013AFE42706A5.txt')
-  pkiValidation(@Res() res: Response) {
-    res.sendFile(resolve('./6DC64225FDA8C517652013AFE42706A5.txt'));
-  }
-
   @Public()
   @Get('hello')
   getHello() {
